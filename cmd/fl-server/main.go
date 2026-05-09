66129031103666b1aeb45425906e18335b924f0d
Charan Sai <guttulacharansai@gmail.com>
2026-05-09 11:59:34 +0530

    Add concurrent connections with Goroutines

--- cmd/fl-server/main.go
+++ cmd/fl-server/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
+	"fmt"
+	"net"
+	"os"
 )
+
+func handleConnection(conn net.Conn) {
+    //Here we are using the net.Conn because it is just a go wrapper around OS - Lwevel Socket
+    //For example , using the net.Conn, we could speak using conn.Write() and listen using conn.Read()
+    //Establishing a live connection over Wifi between Clients RAM and Server's RAM  
+    buffer := make([]byte,1024)
+
+    n,err := conn.Read(buffer)
+
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    // Converting the buffer into byte slices
+    buffer_byte := string(buffer[:n])
+    fmt.Printf(buffer_byte)
+
+    
+    //Closing the connection as its directly connected with the OS resurces , just to prevent the Memory leaks
+    conn.Close()
+}
 
 func main() {
     ln, err := net.Listen("tcp", ":8080")
@@ -25,6 +47,8 @@ func main() {
         }
 
         fmt.Println("New Clinet connected")
-        conn.Close()
+
+        go handleConnection(conn)
+        // the handleConnection handles the connection , so the close Connection is not required here anymore
     }
 }
